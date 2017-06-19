ca23a74f186eb1ef98e00fecbdbdda0b577c8be0
Cameron Wong <cjwong@andrew.cmu.edu (lancer)>
2017-06-19 01:31:33 -0400

    use the real env name lol

--- feubot.py
+++ feubot.py
@@ -125,4 +125,4 @@ async def doot():
 <:doot:324593825815461889> :trumpet: <:doot:324593825815461889> :trumpet: <:doot:324593825815461889> :trumpet: <:doot:324593825815461889> :trumpet: <:doot:324593825815461889> :trumpet: <:doot:324593825815461889> :trumpet: :trumpet: <:doot:324593825815461889> :trumpet:
 <:doot:324593825815461889> <:doot:324593825815461889> :trumpet: :trumpet: :trumpet: <:doot:324593825815461889> :trumpet: :trumpet: :trumpet: <:doot:324593825815461889> :trumpet: :trumpet: :trumpet: <:doot:324593825815461889> :trumpet:""")
 
-bot.run(sys.environ['APP_TOKEN'])
+bot.run(sys.environ['TOKEN'])

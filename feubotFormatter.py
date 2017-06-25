5454e4ae2fbdcd57766ed493322c367709db7aff
Crazycolorz5 <crazycolorz5@gmail.com>
2017-06-25 00:16:37 -0400

    Made work for upper/lower cases, rather than putting all upper case before lower case.

--- feubotFormatter.py
+++ feubotFormatter.py
@@ -2,4 +2,5 @@ from discord.ext.commands import HelpFormatter
 
 class FeubotFormatter(HelpFormatter):
     def filter_command_list(self):
-        return sorted(super().filter_command_list())
+        return sorted(super().filter_command_list(), key=lambda e: str.lower(e[0]))
+

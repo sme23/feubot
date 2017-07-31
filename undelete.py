94ecff52d7c029c00d4d8c2ccb49d4200d60a70d
Cameron Wong <cjwong@andrew.cmu.edu (lancer)>
2017-07-30 20:21:48 -0400

    per channel

--- undelete.py
+++ undelete.py
@@ -9,36 +9,40 @@ _PERMITTED_ROLES = ['Paladins', 'Lords', 'Mage Knights']
 
 _cache = None
 
-class DeletedCache(dict):
-    def __getitem__(self, key):
+class DeletedCache(object):
+    __slots__ = ['channels']
+
+    def insert(self, msg):
         reqt = time()
-        try:
-            result, ts = dict.__getitem__(self, key)
-        except KeyError:
-            return None
+        key = msg.channel.id
+        if key not in self.channels:
+            self.channels[key] = [(msg, reqt)]
         else:
-            if reqt-ts >= _TIMEOUT:
-                self.__delitem__(key)
-                return None
-            return result
+            self.channels[key].append((msg, reqt))
+            self._prune(key, reqt)
 
-    def __setitem__(self, key, value):
-        addt = time()
-        self.mostrecent = (value, addt)
-        dict.__setitem__(self, key, (value, addt))
-
-    def last(self):
+    def last(self, chn, n, name=None):
         reqt = time()
-        if self.mostrecent is not None:
-            result, ts = self.mostrecent
-            if reqt-ts >= _TIMEOUT:
-                self.mostrecent = None
-                return None
-            return result
-        return None
+        self._prune(chn.id, reqt)
+        if chn.id not in self.channels: return None
+        s = [msg for msg, ts in self.channels[chn.id]
+                if name is None or msg.author.name == name][-n:]
+        if not s: return None
+        n = len(s)
+        result = (("%s deleted %d message(s) within the last minute:\n"
+                    % (name,n))
+                if name is not None
+                else ("%d deleted message(s) within the last minute:\n" % n))
+        return result + '```' + '\n'.join(m.content for m in s) + '```'
 
     def __init__(self):
-        self.mostrecent = None
+        self.channels = dict()
+
+    def _prune(self, key, reqt):
+        if key not in self.channels: return
+        self.channels[key] = [
+            (msg, ts) for msg, ts in self.channels[key] if reqt-ts <= _TIMEOUT
+        ]
         super().__init__()
 
 class UndeleteCog(object):
@@ -46,7 +50,7 @@ class UndeleteCog(object):
     __slots__ = ['bot']
 
     @bot.command(pass_context=True, hidden=True)
-    async def undelete(self, ctx, *, name=None):
+    async def undelete(self, ctx, n=1, name=None):
         msg = ctx.message
         if (type(msg.author) is discord.Member
             and not [r for r in msg.author.roles
@@ -54,20 +58,15 @@ class UndeleteCog(object):
         ):
             await self.bot.say('You do not have undelete permissions.')
             return
-        if name is None:
-            if _cache.last() is not None:
-                await self.bot.say(
-                        UndeleteCog.FORMAT.format(_cache.last()))
-            else:
-                await self.bot.say(
-                        'No messages deleted within the last minute')
-            return
-        if _cache[name] is not None:
-            await self.bot.say(UndeleteCog.FORMAT.format(_cache[name]))
+        if n < 1: return
+        result = _cache.last(ctx.message.channel, n, name)
+        if _cache.last(ctx.message.channel, n, name) is not None:
+            await self.bot.say(result)
         else:
             await self.bot.say(
-                    'No messages deleted by %s within the last minute'
-                    % name)
+                'No messages deleted ' +
+                ('by %s ' % name if name is not None else '') +
+                'within the last minute')
 
     def __init__(self, bot):
         global _cache
@@ -75,9 +74,7 @@ class UndeleteCog(object):
         self.bot = bot
 
 def cache(msg):
-    if type(msg.author) is discord.User:
-        print('Caching deletion from %s' % msg.author.name)
-    _cache[msg.author.name] = msg
+    _cache.insert(msg)
 
 def setup(bot):
     bot.add_cog(UndeleteCog(bot))

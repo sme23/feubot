7e60ac826595e54f794861a2b2e66359377d7472
Bovie Xiong <bovie.xiong@gmail.com>
2018-03-18 14:04:31 +0000

    repo update

--- helpful.py
+++ helpful.py
@@ -80,7 +80,7 @@ class Helpful:
     @bot.command()
     async def repository(self):
         """graphics for you"""
-        embed=discord.Embed(title="Animation Repository", url='https://docs.google.com/spreadsheets/d/1JVVgtmLt1eL5M7WcRwCXmybZ7KroAU5ZajNu6fjXwSY/edit#gid=1839610866', description="Get your animations here (credits missing on many, check just in case!)", color=0x4286f4)
+        embed=discord.Embed(title="Animation Repository", url='https://drive.google.com/drive/folders/1s7dXV4OyzjuKYwb2lOKzyyj1Nvb0DajA', description="Get your animations here (credits missing on many, check just in case!)", color=0x4286f4)
         await self.bot.say(embed=embed)
 
     @bot.command()

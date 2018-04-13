0bd7cf9e03276b3033fcbf44dfd08de9e67a0ca4
U-COLORZ-STRIX\Crazycolorz5 <Crazycolorz5@gmail.com>
2018-04-13 00:06:45 -0400

    Debugged file saving/loading.

--- other.py
+++ other.py
@@ -1,26 +1,38 @@
 from discord.ext import commands
 import os
 from pickle import dump, load
+import pickle
+from functools import reduce
 
 class Other:
     """Commands added for convienience."""
     def __init__(self, bot):
         self.bot = bot
-        if os.path.exists("commands.pickle"):
-            with open('data.pickle', 'rb') as f:
-                try:
-                    self.dynamicCommands = pickle.load(f)
+        try:
+            if os.path.exists("commands.pickle"):
+                with open('commands.pickle', 'rb') as f:
+                    self.dynamicCommands = load(f)
                     if type(self.dynamicCommands) != dict():
                         raise Exception
-                    for command in self.dynamicCommands:
-                        @self.bot.command(name = command, cog_name = "Other")
-                        async def local():
-                            await self.bot.say(self.dynamicCommands[command])
-                except Exception:
-                    print("Corrupted command pickle file! Loading nothing.")
-                    self.dynamicCommands = dict()
+        except Exception as e:
+            try:
+                if os.path.exists("commands_backup.pickle"):
+                    with open('commands_backup.pickle', 'rb') as f:
+                        self.dynamicCommands = load(f)
+                        if type(self.dynamicCommands) != dict():
+                            raise Exception
+                else:
+                    print("Corrupted command pickle file! Loading nothing.\nException: %s" % e)
+            except Exception as e2:
+                print("Corrupted command pickle file and backup! Loading nothing.\nException: %s\n%s" % (e, e2))
+            self.dynamicCommands = dict()
         else:
             self.dynamicCommands = dict()
+
+        for command in self.dynamicCommands:
+            @self.bot.command(name = command, cog_name = "Other")
+            async def local():
+                await self.bot.say(self.dynamicCommands[command])
 
     @commands.command(ignore_extra = False)
     @commands.has_permissions(administrator = True)
@@ -54,11 +66,22 @@ class Other:
         try:
             with open('commands.pickle', 'wb') as f:
                 # Pickle the 'data' dictionary using the highest protocol available.
-                pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
-        except Exception:
-            await self.bot.say("Error saving commands.")
+                dump(self.dynamicCommands, f, pickle.HIGHEST_PROTOCOL)
+
+            with open('commands_backup.pickle', 'wb') as f:
+                dump(self.dynamicCommands, f, pickle.HIGHEST_PROTOCOL)
+        except Exception as e:
+            await self.bot.say("Error saving commands.\nException: %s" % e)
             return
         await self.bot.say("Commands successfully saved.")
+
+    @commands.command(name = 'eval')
+    @commands.has_permissions(administrator = True)
+    async def botEval(self, *, arg):
+        """Admins only. Evaluate a Python code segment. UNSAFE!!!"""
+        fix = lambda f: (lambda x: x(x))(lambda y: f(lambda args: y(y)(args)))
+        res = eval(arg, __builtins__, { "fix" : fix , "reduce" : reduce })
+        await self.bot.say(str(res))
 
 
 def setup(bot):
